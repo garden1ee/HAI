950c84cae030617e144509b1b94adfb20e861b75
garden1ee <garden1ee@users.noreply.github.com>
2020-11-18 19:02:15 +0900

    'f'

---
+++ pages/processor.js
@@ -0,0 +1,21 @@
+let processor = {
+    doLoad: function() {
+      this.inpaint_canvas = document.getElementById("canvas");
+      this.inpaint_ctx = this.inpaint_canvas.getContext("2d");
+      this.elImage = document.getElementById("userUploadedImage");
+
+      this.elImage.addEventListener("change", (evt)=>{
+          const image = evt.target.files[0];
+          const targetImage = new Image();
+          targetImage.addEventListener("load",(evt)=>{
+            this.inpaint_ctx.drawImage(targetImage,0,0,550,400);
+          });
+          targetImage.src=window.URL.createObjectURL(image);
+      })
+    },
+  
+  };
+
+document.addEventListener("DOMContentLoaded", () => {
+  processor.doLoad();
+});
